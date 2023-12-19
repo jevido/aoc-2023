3fd63b405cf93d834e8dd65ad8f31b98c8fc83c2
jevido <jheremenis@gmail.com>
2023-12-19 16:25:47 +0100

    Ik ben verboos

--- src/routes/day/3/+page.svelte
+++ src/routes/day/3/+page.svelte
@@ -2,6 +2,7 @@
 	async function load() {
 		let answerPart1 = 0;
 		let answerPart2 = 0;
+
 		const text = await (await fetch('3/input-file')).text();
 
 		const lines = text.split('\n');
@@ -57,10 +58,128 @@
 		}
 
 		function save(number) {
-			console.debug(`${answerPart1} + ${number}`);
 			answerPart1 += number * 1;
 		}
+
+		for (const rowIndexString in data) {
+			const rowIndex = Number(rowIndexString);
+			const row = data[rowIndex];
+			// Find gears (*)
+
+			for (const columnIndex in row) {
+				const column = Number(columnIndex);
+				const character = row[column];
+
+				if (character === '*') {
+					const topLeft = data[rowIndex - 1][column - 1];
+					const topMiddle = data[rowIndex - 1][column];
+					const topRight = data[rowIndex - 1][column + 1];
+					const middleLeft = data[rowIndex][column - 1];
+					const middleRight = data[rowIndex][column + 1];
+					const bottomLeft = data[rowIndex + 1][column - 1];
+					const bottomMiddle = data[rowIndex + 1][column];
+					const bottomRight = data[rowIndex + 1][column + 1];
+
+					let topNumbers = [];
+					let middleNumbers = [];
+					let bottomNumbers = [];
+
+					if (isNumber(topLeft)) {
+						const number = scanStringReverse(rowIndex - 1, column - 1);
+						topNumbers.push(number);
+					}
+
+					if (isNumber(topMiddle)) {
+						if (topNumbers.length !== 0) {
+							topNumbers[0] += topMiddle;
+						} else {
+							topNumbers.push(topMiddle);
+						}
+					}
+
+					if (isNumber(topRight)) {
+						const number = scanString(rowIndex - 1, column + 1);
+						if (isNumber(topMiddle)) {
+							topNumbers[0] += number;
+						} else {
+							topNumbers.push(number);
+						}
+					}
+
+					if (isNumber(middleLeft)) {
+						const number = scanStringReverse(rowIndex, column - 1);
+						middleNumbers.push(number);
+					}
+					if (isNumber(middleRight)) {
+						const number = scanString(rowIndex, column + 1);
+						middleNumbers.push(number);
+					}
+
+					// bottom
+					if (isNumber(bottomLeft)) {
+						const number = scanStringReverse(rowIndex + 1, column - 1);
+						bottomNumbers.push(number);
+					}
+
+					if (isNumber(bottomMiddle)) {
+						if (bottomNumbers.length !== 0) {
+							bottomNumbers[0] += bottomMiddle;
+						} else {
+							bottomNumbers.push(bottomMiddle);
+						}
+					}
+
+					if (isNumber(bottomRight)) {
+						const number = scanString(rowIndex + 1, column + 1);
+						if (isNumber(bottomMiddle)) {
+							bottomNumbers[0] += number;
+						} else {
+							bottomNumbers.push(number);
+						}
+					}
+
+					let totalNumbers = [...topNumbers, ...middleNumbers, ...bottomNumbers];
+
+					if (totalNumbers.length == 2) {
+						answerPart2 += totalNumbers[0] * totalNumbers[1];
+						console.debug(totalNumbers[0] * totalNumbers[1], totalNumbers[0], totalNumbers[1]);
+					}
+				}
+			}
+		}
+
 		return [answerPart1, answerPart2];
+
+		function scanStringReverse(row, column) {
+			let number = '';
+
+			for (let breakdown = column; breakdown >= 0; breakdown--) {
+				const char = data[row][breakdown];
+				console.debug(char);
+				if (isNumber(char)) {
+					number = char + number;
+				} else {
+					break;
+				}
+			}
+
+			return number;
+		}
+
+		function scanString(row, column) {
+			let number = '';
+
+			for (let index = column; index < data[row].length; index++) {
+				const char = data[row][index];
+
+				if (isNumber(char)) {
+					number += char;
+				} else {
+					break;
+				}
+			}
+			return number;
+		}
 	}
 
 	function isNumber(char) {
@@ -83,13 +202,7 @@
 
 {#await load()}
 	Calulating...
-{:then [part1]}
-	Answer: {part1}
+{:then [part1, part2]}
+	Answer part 1: {part1} <br />
+	Answer part 2: {part2}
 {/await}
-
-<br /><br />
-I have tried:
-<ul>
-	<li>533784</li>
-	<li>533784</li>
-</ul>
